3c79e63d557b146a1e6f23559e97f80a720faafa
Martikk <alex.martishyn@gmail.com>
2023-10-14 01:18:00 -0700

    Task3.1

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,5 +1,13 @@
-// code
-    let number1 = Math.floor(Math.random()*100
-    )
-    console.log (number1);
-    //code
+// // code
+//     let number1 = Math.floor(Math.random()*100
+//     )
+//     console.log (number1);
+//     //code
+
+let number = 1;
+console.log(number);
+number++;
+console.log(number);
+console.log(number);
+number--;
+console.log(number);
